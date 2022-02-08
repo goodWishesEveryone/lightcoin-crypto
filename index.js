72978a5d515d3d9b9aca9bbb7c00a43edd84d864
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-02-08 09:44:35 -0500

    Modify to include description of LightCoin (LHL) Crypto App

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-// Account class to be able to keep track of the transactions
+////////////////  LightCoin (LHL) Crypto App  ////////////////
+// List of features :
+// Allow multiple accounts to be created
+// Each account can have many transactions
+// Allow withdrawals and deposits into accounts
+// Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+// Allow us to retrieve the current balance of the account at any time
+// Don't allow withdrawals that exceed the remaining balance of the account
 
+
+// Account class to be able to keep track of the transactions
 class Account {
   constructor() {
     this.transactions = [];
